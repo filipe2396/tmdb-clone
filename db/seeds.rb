171aa0f8bfc408740f26a72ae43d58f701dd2ad3
Filipe Pereira <pereira.lipe96@gmail.com>
2021-06-29 14:39:42 -0300

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+unless Movie.any?
+  Movie.create(
+    tmdb_id: 299_534,
+    title: 'Vingadores: Ultimato',
+    release_date: '2019-04-23',
+    overview: 'Após os eventos devastadores de \"Vingadores: Guerra Infinita\", o universo está em ruínas devido aos esforços do Titã Louco, Thanos. Com a ajuda de aliados remanescentes, os Vingadores devem se reunir mais uma vez a fim de desfazer as ações de Thanos e restaurar a ordem no universo de uma vez por todas, não importando as consequências.',
+    poster_path: '/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg',
+    vote_average: 8.3,
+    vote_count: 18_409
+  )
+end
